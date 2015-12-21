f6949caf220789481184d660f2bea2b7d7a1c8be
Matt Chessman <matthewmatics17@gmail.com>
2015-12-21 09:33:41 -0700

    resolving conflicts

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', { title: 'Habbit Rabbit' });
 });
 
 router.get('/:id', function(req, res, next) {
