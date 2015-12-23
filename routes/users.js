8be140622278ee151cddbf4903279a559c827362
hhoburg <hhoburg@gmail.com>
2015-12-22 18:51:02 -0700

    made some functions for habit tracking or whatever

--- routes/users.js
+++ routes/users.js
@@ -1,122 +1,87 @@
 'use strict';
-
 var express = require('express');
 var router = express.Router();
 // reqire pg that interacts with knex
 var pg = require('pg');
-
 var knex = require('knex')({
   client: 'pg',
   connection: process.env.DATABASE_URL
 });
-
-
 var streakArray = [];
-
 function getMaxofArray(numArray) {
   return Math.max.apply(null, numArray);
 }
 
-var data = [{
-  habits: 'coding',
-  streak: '4',
-  streakHabit: 'coding',
-  goal: '10',
-  progressBar: '60'
-}, {
-  habits: 'jogging',
-  streak: '6',
-  streakHabit: 'jogging',
-  goal: '10',
-  progressBar: '80'
-}, {
-  habits: 'breathing',
-  streak: '3',
-  streakHabit: 'breathing',
-  goal: '10',
-  progressBar: '10'
-}, {
-  habits: 'eating',
-  streak: '10',
-  streakHabit: 'eating',
-  goal: '10',
-  progressBar: '100'
-}];
-
 //********* USER ROUTES***********//
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
+  var userData = {};
+  userData.habits = [];
+  userData.streak = [];
   var id = req.params.id;
   // HABITS DATA REQUEST BASED ON USER ID
   knex.select('*').table('goodhabits').where('userid', id).then(function(success) {
     // NEED TO ADD VIEWS BASED ON DATA RETURNED
+    for (var i = 0; i < success.length; i++) {
+      userData.habits[i] = success[i];
+    }
     console.log(success);
+
+    res.render('show', userData);
   }, function(failure) {
     console.log('The query Failed: ' + failure);
   });
-
   // HABIT LOG DATA REQUEST BASED ON USER ID
   knex.select('*').table('habitlog').where('userid', id).then(function(success) {
     // NEED TO ADD VIEWS BASED ON DATA RETURNED
     console.log(success);
+
+    // console.log(Date.parse(success[0].logdate))
+    userData.streak = [];
+    // for (var i = 0; i < success.length; i++) {
+    //   for (var j = 0; j < success.length; j++) {
+    //     if (success[i].habitid === success[j].habitid && success[i].id !== success[j].id) {
+    //       var dateOne = Date.parse(success[j].logdate);
+    //       // console.log(dateOne);
+    //       var dateTwo = Date.parse(success[i].logdate);
+    //       var dateDiff = (dateOne-dateTwo)/3600000
+    //       console.log(dateDiff);
+    //       var streak = Math.floor(Date.parse(dateDiff))
+    //       // console.log(streak);
+    //       userData.streak.push(streak);
+    //     }
+    //   }
+    //   // var next = i + 1;
+    //   // console.log(next);
+    //   // var nextDate = Date.parse(success[next].logdate);
+    //   // var firstDate = Date.parse(success[i].logdate);
+    //   // userData.streak.push(nextDate - firstDate)
+    // }
+    // success.reduce()
+    for (var i = 0; i < success.length; i++) {
+      Date.parse(success[i].logdate)
+    }
   }, function(failure) {
     console.log('The query Failed: ' + failure);
   });
 
-  var userData = {
-    name: "Alya",
-    habits: data,
-    // streak:
-    // longestStreak: longStreak
-  };
-
-  res.render('show', userData);
 });
-
 router.get('/', function(req, res, next) {
-
   res.render('users', {
     title: 'Habbit Rabbit'
   });
-
 });
-
-<<<<<<< HEAD
-router.get('/:id', function(req, res, next) {
-  // var longestStreak = getMaxofArray(streakArray);
-  // var longStreak = getMaxofArray(streakArray);
-  // for (var i = 0; i <= data.length-1; i++) {
-  //   streakArray.push(data[i].streak);
-  //   return streakArray;
-  //   console.log(streakArr);
-  // }
-  var userData = {
-    name: "Alya",
-    habits: data,
-    streak: data[0].streak
-    // streak:
-    // longestStreak: longStreak
-  };
-
-  res.render('show', userData);
-  // var longestStreak = getMaxofArray(streakArray);
-=======
 // Get USER by ID
 router.get('/get/:id', function(req, res) {
-
   var id = req.params.id;
   knex.select('*').table('users').where('id', id).then(function(success) {
     console.log(success[0].firstname);
     res.write(success[0].firstname);
     res.end();
-
   }, function(failure) {
     console.log('The query Failed: ' + failure);
   });
-
->>>>>>> 14a51ae694ca5fc005f873f77654f77506d38aa2
 });
-
 // Create User /users/create
 router.post('/create', function(req, res) {
   var user = {};
@@ -126,37 +91,27 @@ router.post('/create', function(req, res) {
   user.lastname = req.body.lastName;
   user.email = req.body.userEmail;
   user.phone = req.body.phoneNum;
-
   user.username = req.body.newUsername;
   user.password = req.body.newPassword;
-
   res.write('this is the new page');
   res.end();
-
   // Write queries to interact with postgres
   knex('users').insert(user).then(function(success) {
     res.end('You Have signed up');
   }, function(failure) {
     console.log(failure);
   });
-
-
   // NEED TO HAVE VIEW MADE FOR THIS
-
 }, function(failure) {
-
   res.write('this is the new page and you failed');
   res.end();
-
 });
-
 // ***** HABBIT ROUTES ******//
 // The naming convention might be a little not good
 // Create a habit for a user
 router.post('/habits/create/:userid', function(req, res) {
   var userid = req.params.userid;
   var habit = {};
-
   //Set Object to have the key value pairs for the query
   habit.userid = userid;
   habit.habitname = req.body.habitname;
@@ -165,8 +120,6 @@ router.post('/habits/create/:userid', function(req, res) {
   habit.duration = 720;
   habit.reminderfreq = 24;
   habit.remindertype = req.body.notification;
-
-
   // INSERT INTO goodhabits VALUES(default, 1, 'Code', 'Code Every day', 24, 5000, 24, 'text');
   // Write queries to interact with postgres
   knex('goodhabits').insert(habit).then(function(success) {
@@ -175,32 +128,23 @@ router.post('/habits/create/:userid', function(req, res) {
     });
   }, function(failure) {
     console.log(failure);
-
   });
-
 }, function(failure) {
   console.log('You failed to retrive route: ' + failure);
 });
-
 // Log a habit
 router.post('/habits/log/:userid/:habitid', function(req, res) {
   var userid = req.params.userid;
   var habitid = req.params.habitid;
   var log = {};
-
   log.userid = userid;
   log.habitid = habitid;
-
   knex('habitlog').insert(habit).then(function(success) {
     console.log(success);
   }, function(failure) {
     console.log(failure);
-
   });
-
 });
-
-
 // Get habits based on user ID
 router.get('/habits/get/:userid/:habitid', function(req, res) {
   var userid = req.params.userid;
@@ -210,27 +154,20 @@ router.get('/habits/get/:userid/:habitid', function(req, res) {
 }, function(failure) {
   console.log('You Failed: ' + failure);
 });
-
 router.get('/:id/habits/create', function(req, res, next) {
   res.render('createHabit', {
     title: 'Habbit Rabbit'
   });
 });
-
 router.post('/:id/habits/create', function(req, res, next) {
   console.log(req.body);
   res.render('createHabit', {
     title: 'Habbit Rabbit'
   });
 });
-
 // get habits based on user and habbit id
 router.get('/habits/get/:userid/:habitid', function(req, res) {
-
 }, function(failure) {
   console.log('You Failed: ' + failure);
 });
-
-
-
 module.exports = router;
